f718a9c1b75f7adf9679bc2ceb0f0594bfe6a135
Jeremy Schaffer <jeremys@codenexus.org>
2017-02-17 00:21:50 -0800

    Fix loading of Laravel Collective HtmlServiceProvider.

--- config/app.php
+++ config/app.php
@@ -163,7 +163,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Laracasts\Flash\FlashServiceProvider::class,
-        // Collective\Html\HtmlServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Package Service Providers...
