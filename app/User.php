1df85f27631522ef68cfc3ff56e55b94cc25cafa
Jeremy Schaffer <jeremys@codenexus.org>
2017-02-20 00:06:01 -0800

    Fix Laravel Dusk issues.

--- app/User.php
+++ app/User.php
@@ -2,7 +2,7 @@
 
 namespace App;
 
-use App\Traits\UserRoles;
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
