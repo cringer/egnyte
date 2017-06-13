95605ccce06c5963d1929231d9bccf4d94aca32f
Jeremy Schaffer <jeremys@codenexus.org>
2017-06-13 07:15:08 -0700

    Work on order status.

---
+++ app/Http/Controllers/Admin/AssignmentStatusController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\AssignmentStatus;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AssignmentStatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $assignmentstatuses = AssignmentStatus::all();
+
+        return view('admin.assignmentstatus.index', compact('assignmentstatuses'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.assignmentstatus.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate(request(), [
+            'name' => 'required'
+        ]);
+
+        $assignmentstatus = AssignmentStatus::create([
+            'name' => $request->name
+        ]);
+
+        flash()->success("$assignmentstatus->name has been created!");
+
+        return redirect()->route('admin.assignmentstatus.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
