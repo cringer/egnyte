ce26e2a62c94b2827f31050834af084ee519a50f
Jeremy Schaffer <jeremys@codenexus.org>
2017-06-27 10:08:26 -0700

    Add soft delete capability to  table.

--- app/NewHire.php
+++ app/NewHire.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class NewHire extends Model
 {
+    use SoftDeletes;
+    
     /**
      * The attributes that are not mass assignable.
      *
