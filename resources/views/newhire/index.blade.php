12ead923fb4619a3c133988846a2709ebb46ac85
Jeremy Schaffer <jeremys@codenexus.org>
2017-07-19 08:28:35 -0700

    Add some newhire page UI tweaks.

--- resources/views/newhire/index.blade.php
+++ resources/views/newhire/index.blade.php
@@ -6,46 +6,43 @@
         <h3>New Hires</h3>
 
         @foreach ($newhires as $newhire)
-{{--             <div class="row">
-                <div class="col-md-8" style="border: 5px solid mistyrose;"> --}}
-                    <article class="flex" style="border: 3px solid {{ $newhire->position->color }};">
-                        <div class="box1 flex">
-                            <a href="newhire/{{ $newhire->slug }}" style="font-size: 16px">
-                                <span style="font-size: 20px">{{ $newhire->name }}</span><br>
-                                {{ $newhire->position->name }} <span class="label" style="background-color:{{ $newhire->position->color }}">{{ $newhire->position->acronym }}</span><br>
-                                Start Date: {{ $newhire->hire_date}}
-                            </a>
-                        </div>
-                        <div class="box box2 flex">
-                            @if (count($newhire->assignment))
-                                    @if ($newhire->assignment->method->id == 1)
-                                        @if (count($newhire->assignment->orders))
-                                            @foreach ($newhire->assignment->orders as $order)
-                                                <a href="/order/{{ $order->id }}" target="_blank">
-                                                    <div class="flex iconbox">
-                                                        <i class="fa fa-shopping-cart fa-2x"></i>
-                                                    </div>
-                                                </a>
-                                            @endforeach
-                                        @else
-                                            <div class="flex iconbox">
-                                                <i class="fa fa-exclamation-triangle fa-2x warning"></i>
-                                            </div>
-                                        @endif
-                                    @else
+            <article class="flex" style="border: 1px solid #ccc;">
+                <div class="box1 flex">
+                    <a href="newhire/{{ $newhire->slug }}" style="font-size: 16px">
+                        <span style="font-size: 18px">{{ $newhire->name }}</span><br>
+                        {{ $newhire->position->name }} <span class="label" style="background-color:{{ $newhire->position->color }}">{{ $newhire->position->acronym }}</span><br>
+                        Start Date: {{ $newhire->hire_date}}
+                    </a>
+                </div>
+                <div class="box box2 flex">
+                    @if (count($newhire->assignment))
+                        @if ($newhire->assignment->method->id == 1)
+                            @if (count($newhire->assignment->orders))
+                                @foreach ($newhire->assignment->orders as $order)
+                                    <a href="/order/{{ $order->id }}" target="_blank">
                                         <div class="flex iconbox">
-                                            <i class="fa fa-check fa-2x complete"></i>
+                                            <i class="fa fa-shopping-cart fa-2x"></i>
                                         </div>
-                                    @endif
-                                @else
-                                    <div class="flex iconbox">
-                                        <i class="fa fa-question fa-2x"></i>
-                                    </div>
-                                @endif
+                                    </a>
+                                @endforeach
+                            @else
+                                <div class="flex iconbox">
+                                    <i class="fa fa-exclamation-triangle fa-2x warning"></i>
+                                </div>
+                            @endif
+                        @else
+                            <div class="flex iconbox">
+                                <i class="fa fa-check fa-2x complete"></i>
+                            </div>
+                        @endif
+                    @else
+                        <div class="flex iconbox">
+                            <i class="fa fa-question fa-2x"></i>
                         </div>
-                    </article>
-{{--                 </div>
-            </div> --}}
+                    @endif
+                </div>
+            </article>
+
         @endforeach
 
             {{ $newhires->links() }}
