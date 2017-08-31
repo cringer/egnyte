553572e184a30255fc2b4fdb62fd1bd90fb721ea
Jeremy Schaffer <jeremys@codenexus.org>
2017-08-31 16:11:48 -0700

    Fix spacing in paramater definition.

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -25,7 +25,7 @@
             'csrfToken' => csrf_token(),
         ]) !!};
     </script>
-    @routes
+    <!-- @routes -->
 </head>
 <body>
     <div id="app">
@@ -62,7 +62,7 @@
 
     <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <script src="https://unpkg.com/vue"></script>
